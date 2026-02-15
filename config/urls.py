8c9c1d28a3ca4e78136f77dff0fd817295982af1
Javohir Gulyamov <gulamovzavohir04@gmail.com>
2026-02-15 23:41:45 +0400

    Add homepage route

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Jobboard API is running 🚀"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +12,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/accounts/', include('accounts.urls')),
+    path('', home),
 
 ]
