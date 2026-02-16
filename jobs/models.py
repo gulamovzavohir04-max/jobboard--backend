f1d7e377084e17a6f1c559f73d32c79370d6efb7
Javohir Gulyamov <gulamovzavohir04@gmail.com>
2026-02-16 12:24:43 +0400

    Add owner field

--- jobs/models.py
+++ jobs/models.py
@@ -1,12 +1,15 @@
+from django.conf import settings
 from django.db import models
 
 class Job(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="jobs"
+    )
     title = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
     description = models.TextField()
-    salary = models.IntegerField()
+    salary = models.IntegerField(null=True, blank=True)
     location = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
